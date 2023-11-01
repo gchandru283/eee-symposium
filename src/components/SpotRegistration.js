53a594d0fc0fa1af977c2cb50ed06c63df0ac7e0
eknasympo <146112183+eknasympo@users.noreply.github.com>
2023-11-01 12:49:43 +0530

    Update SpotRegistration.js
    changed g-form link

--- src/components/SpotRegistration.js
+++ src/components/SpotRegistration.js
@@ -9,7 +9,7 @@ const toggleHover = ()=>{
     <div className=" lg:text-2xl md:text-xl text-white fixed bg-black pt-16 pb-3 bg-opacity-80 w-full text-center z-0">
     <div className={`${mouseHover ? "blinking-text" : ""} `}
     onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
-      For Onspot Registration - <a className="text-blue-300 cursor-pointer" href="https://docs.google.com/forms/d/e/1FAIpQLSdfOFgS2KbyfGFu97P2rhjouORbEgFwCk8CtGO_QNojuPQWHg/viewform?usp=sf_link" target="blank">Click Here!</a>
+      For Onspot Registration - <a className="text-blue-300 cursor-pointer" href="https://docs.google.com/forms/d/e/1FAIpQLSfCpLKgd4mlIyLpYlPhMHS8esehUSRWYsAtGEK4jJ2iCAy8Ow/viewform?usp=pp_url" target="blank">Click Here!</a>
     </div></div>
   );
 };
