42d9103f50214d3024a80d27bc8ff80e70381e99
Chandru G <117466812+gchandru283@users.noreply.github.com>
2023-11-02 15:47:45 +0530

    Update SpotRegistration.js

--- src/components/SpotRegistration.js
+++ src/components/SpotRegistration.js
@@ -7,9 +7,9 @@ const toggleHover = ()=>{
 }
   return (
     <div className=" lg:text-2xl md:text-xl text-white fixed bg-black pt-16 pb-3 bg-opacity-80 w-full text-center z-0">
-    <div className={`${mouseHover ? "blinking-text" : ""} `}
+    <div className={cursor-pointer `${mouseHover ? "blinking-text" : ""} `}
     onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
-      For Onspot Registration - <a className="text-blue-300 cursor-pointer" href="https://docs.google.com/forms/d/e/1FAIpQLSfCpLKgd4mlIyLpYlPhMHS8esehUSRWYsAtGEK4jJ2iCAy8Ow/viewform?usp=pp_url" target="blank">Click Here!</a>
+      Registrations Closed, No Onspot Registration on 07.11.2023!
     </div></div>
   );
 };
