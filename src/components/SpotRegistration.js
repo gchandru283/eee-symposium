65722244ee3e162fa19e9e9c50d00a928349d7da
“gchandru283” <gchandru283@gmail.com>
2023-11-02 18:22:32 +0530

    updation

--- src/components/SpotRegistration.js
+++ src/components/SpotRegistration.js
@@ -6,8 +6,8 @@ const toggleHover = ()=>{
     setMouseHover(!mouseHover);
 }
   return (
-    <div className=" lg:text-2xl md:text-xl text-white fixed bg-black pt-16 pb-3 bg-opacity-80 w-full text-center z-0">
-    <div className={cursor-pointer `${mouseHover ? "blinking-text" : ""} `}
+    <div className=" lg:text-2xl md:text-xl text-white font-semibold fixed bg-black pt-16 pb-3 bg-opacity-80 w-full text-center z-0">
+    <div className={ `cursor-pointer ${mouseHover ? "blinking-text" : ""} `}
     onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
       Registrations Closed, No Onspot Registration on 07.11.2023!
     </div></div>
